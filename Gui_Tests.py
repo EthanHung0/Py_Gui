3a5ed002d0dd3d9574952d2e4be40f96f3ae68a5
EthanHung0 <aaronoriginal2009@gmail.com>
2025-01-11 18:47:46 +0700

    no message

--- Gui_Tests.py
+++ Gui_Tests.py
@@ -3,9 +3,9 @@ from guizero import App, PushButton
 app_ = App(title="hello", width=400, height=250, layout="grid", bg=(0, 0, 0))
 
 def press_check():
-    print("Pressed.")
+    print("Nhangay")
 
-button = PushButton(app_, text="Calm.", command=press_check,grid=[1,1])
+button = PushButton(app_, text="Button Of Truth", command=press_check,grid=[1,1])
 button.bg = (255, 255, 255)
 button.text_color = (0, 0, 0)
 button.text_bold = True
